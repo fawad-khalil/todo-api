6d655fe65ad4a47e0c90669c3fa6aefb451103c5
Fawad Khalil <fawadkhalil@MacBook-Pro.local>
2020-08-26 03:24:05 +0500

    Add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// Refer to: https://prettier.io/docs/en/options.html
+
+module.exports = {
+	trailingComma: 'es5',
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	'jsx-single-quote': true,
+	'jsx-bracket-same-line': true,
+	'arrow-parens': 'always',
+	printWidth: 120,
+	useTabs: true,
+};
