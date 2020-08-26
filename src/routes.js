b2a8f0a93b8fc77a4fcdeb6d1146b163d6e642b7
Fawad Khalil <fawadkhalil@MacBook-Pro.local>
2020-08-27 00:11:35 +0500

    add user, todo routers

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 const sessionRouter = require('./modules/session/routes');
+const userRouter = require('./modules/user/routes');
+const todoRouter = require('./modules/todo/routes');
 
 module.exports = {
 	sessionRouter,
+	userRouter,
+	todoRouter,
 };
